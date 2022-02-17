4d28467b5f42499ae58baf1607928ecca0b12ca5
shivnathyadav <shivnath.yadav@zemosolabs.com>
2022-02-17 11:12:23 +0530

    dockerfile added

--- backend/user-management/src/main/java/com/bootcamp4/usermanagement/UserManagementApplication.java
+++ backend/user-management/src/main/java/com/bootcamp4/usermanagement/UserManagementApplication.java
@@ -2,8 +2,8 @@ package com.bootcamp4.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
